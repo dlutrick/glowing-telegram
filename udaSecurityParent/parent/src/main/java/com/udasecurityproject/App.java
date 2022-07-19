e4ef14eab4cfd9f8d7234d6641555f22e8f62d82
David <davidrlutrick@gmail.com>
2022-07-18 21:01:16 -0700

    added starter files

---
+++ udaSecurityParent/parent/src/main/java/com/udasecurityproject/App.java
@@ -0,0 +1,13 @@
+package com.udasecurityproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
